9a96c6ba8c2db9c5fe32671e92e9b6e5cfa621ae
fwappy <Ezra.kainz@gmail.com>
2023-12-03 14:27:48 -0800

    reimplement algorithm

--- main.cpp
+++ main.cpp
@@ -50,60 +50,82 @@ int main() {
        if (res == CURLE_OK) {
            try {
                // Parse the JSON data using nlohmann/json
-               json earthquakes = json::parse(response);
-               earthquakes = earthquakes["features"];
+               json parsed = json::parse(response);
+               json earthquakes[0] = parsed["features"];
 
+               n = 0;
+               while true {
+                   // if earthquakes[n] is empty, exit loop and print statistics
+                   if (earthquakes[n].empty()) {
+                       break;
+                }
 
-              // std::cout << earthquakes.dump(4) << std::endl;
+                // filter out earthquakes not within searchDistance of another earthquake
+                // optimisation would be to just run this once at start, then only check if the first earthquake on later interations
+                json earthquakesFiltered;
+                for (int i = 0; i < (earthquakes[n].size() - 1); i++) {
+                    for (int j = 0 + 1; j < earthquakes[n].size(); j++) {
+                        if (j == i) {
+                            j++;
+                        }
 
+                        double a = earthquakes[n][i]["geometry"]["coordinates"][0];
+                        double b = earthquakes[n][i]["geometry"]["coordinates"][1];
+                        double x = earthquakes[n][j]["geometry"]["coordinates"][0];
+                        double y = earthquakes[n][j]["geometry"]["coordinates"][1];
 
-               // Filter out Points not within searchDistance of another point
-                   // Select First point
-
-                   // check distance with Pythagorean Therom to points with larger index
-                   // if distance < searchDistance copy both points to new json
-                   // select next point
-
-               auto size = earthquakes.size();
-              
-               json filteredEarthquakes[size/2];
-
-               for(int i=0; i<size-1; i++){ 
-                   for(int j=i+1; j<size; j++){
-
-
-                       double a = earthquakes[i]["geometry"]["coordinates"][0];
-                       double b = earthquakes[i]["geometry"]["coordinates"][1];
-                       double x = earthquakes[j]["geometry"]["coordinates"][0];
-                       double y = earthquakes[j]["geometry"]["coordinates"][1];
-
-
-                        
-                      
-                       if(haversineDistance(a, b, x, y) < searchDistance){
-                           filteredEarthquakes[0].push_back(earthquakes[i]);            
-                           break;
+                        // check distance
+                        if  (haversineDistance(a, b, x, y) < searchDistance) {
+                            earthquakesFiltered.push_back(earthquakes[n][i]);
+                            break;
                        }
-                 
-                   }
-                  
-               }
-                   std::cout << filteredEarthquakes[0].dump(4) << std::endl;
+                    }
+                }
+
+
+                double avgLocation[2];
+                avgLocation[0] = earthquakesFiltered[1]["geometry"]["coordinates"][0];
+                avgLocation[1] = earthquakesFiltered[1]["geometry"]["coordinates"][1];
 
 
 
-               // Find center of group of quakes
-                   // Select First point
-                   // check distance with pithagoream theorum to points with larger index
-                   // if distance < searchDistance copy points to new swarm[n]
 
-                   // find average location of swarm
-                   // clear swarm[n]
-                   // check distance to all earthquakes, copy earthquakes closer than search distance to swarm[n]
-                   
-                   // repeat above 3 steps until average location stops changing
-                   // check distance to all earthquakes, copy earthquakes not within distance to new 
+                while true {
+                    // check distance to each earthquake in earthquakesFiltered
+                    for (int i = 0; i < earthquakesFiltered.size(); i++) {
+                        double x = earthquakesFiltered[i]["geometry"]["coordinates"][0];
+                        double y = earthquakesFiltered[i]["geometry"]["coordinates"][1];
 
+                        if (haversineDistance(avgLocation[0], avgLocation[1], x, y) > searchDistance){
+                            swarm[n].push_back(earthquakesFiltered[i])
+
+                        } else {
+                            earthquakes[n+1].push_back(earthquakesFiltered[i]);
+                        }
+                    }
+
+                    // Calculate average location of swarm [n]
+                    double newAvgLocation[2] = 0;
+                    for (int i = 0; i < swarm[n].size(); i++) {
+                        newAvgLocation[0] = newAvgLocation[0] + swarm[i]["geometry"]["coordinates"][0];
+                        newAvgLocation[1] = newAvgLocation[1] + swarm[i]["geometry"]["coordinates"][1];
+                    }
+
+                    newAvgLocation[0] = newAvgLocation[0] / swarm[n].size();
+                    newAvgLocation[1] = newAvgLocation[1] / swarm[n].size();
+
+                    if (newAvgLocation[0] == avgLocation[0] && newAvgLocation[1] == avgLocation[1]){
+                        break;
+                    }
+                    avgLocation = newAvgLocation;
+                    quakes[n].clear();
+                    earthquakes[n+1].clear();
+                }
+                n++;
+            }
+
+
+            // print statistics here
 
            } catch (const std::exception& e) {
                std::cerr << "JSON parsing error: " << e.what() << std::endl;
